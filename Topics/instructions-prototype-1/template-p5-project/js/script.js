01ae8961eb85cda9ca2a79c46e4ac2ce310e5087
yogurtdairy <nicole.tanev.n19@gmail.com>
2026-09-23 20:50:47 -0400

    added files for prototype 2

--- Topics/instructions-prototype-1/template-p5-project/js/script.js
+++ Topics/instructions-prototype-1/template-p5-project/js/script.js
@@ -1,24 +1,45 @@
-/**
- * Title of Project
- * Author Name
- * 
- * HOW EMBARRASSING! I HAVE NO DESCRIPTION OF MY PROJECT!
- * PLEASE REMOVE A GRADE FROM MY WORK IF IT'S GRADED!
- */
+let value = 100;
 
-"use strict";
-
-/**
- * OH LOOK I DIDN'T DESCRIBE SETUP!!
-*/
 function setup() {
-
+  createCanvas(600, 600);
 }
 
-
-/**
- * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
-*/
 function draw() {
+  background(220);
+  slotRolling();
+  rectMode(CENTER);
+  // rect(150, 200, 250, 200);
+  push();
+  fill(255, 0, 0);
+  noStroke();
+  ellipse(500, value, 40);
+  pop();
+  push();
+  noStroke();
+  fill(255, 120, 120);
+  ellipse(498, value - 3, 18, 22);
+  pop();
+}
 
+function slotRolling() {
+  if (value > 200) {
+    //this is where code for slot rolling will begin
+    textAlign(CENTER, CENTER);
+    textSize(20);
+    text("🌸", 50, 50);
+  }
+  if (value < 200) {
+    textAlign(CENTER, CENTER);
+    textSize(20);
+    text(" ", 50, 50);
+  }
+}
+
+function mousePressed() {
+  //console.log("mouse click");
+  console.log(value);
+  value += 200;
+  if (value > 300) {
+    value = 100;
+  }
 }
