2ccee14e019bd0c60e86d7cb953b4af2ac4e6e76
Nicole Tanev <nicole.tanev.n19@gmail.com>
2026-09-23 21:38:54 -0400

    added prototype 3 code

--- Topics/insctructions-prototype-3/template-p5-project/js/script.js
+++ Topics/insctructions-prototype-3/template-p5-project/js/script.js
@@ -1,24 +1,62 @@
-/**
- * Title of Project
- * Author Name
- * 
- * HOW EMBARRASSING! I HAVE NO DESCRIPTION OF MY PROJECT!
- * PLEASE REMOVE A GRADE FROM MY WORK IF IT'S GRADED!
- */
-
-"use strict";
-
-/**
- * OH LOOK I DIDN'T DESCRIBE SETUP!!
-*/
 function setup() {
-
+  createCanvas(400, 400);
 }
 
-
-/**
- * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
-*/
 function draw() {
+  noCursor();
+  background("#03b5aa");
+  
+  push();
+  strokeWeight(5);
+  line(mouseX, mouseY - 20, 200, 80);
+  pop();
+  
+  push();
+  stroke(255, 220, 200);
+  fill(255, 255, 255);
+  ellipse(200, 70, 20, 40);
+  pop();
+  
+  push();
+  stroke("#f1bea1");
+  strokeWeight(30);
+  line(200, 70, 200, 0);
+  pop();
+  
+  push();
+  noStroke();
+  fill("#f6d4c1");
+  ellipse(200, 70, 20, 30);
+  pop();
+  
+  push();
+  fill(200, 0, 0);
+  ellipse(mouseX, mouseY, 40);
+  pop();
+  
+  push();
+  fill(255, 0, 0);
+  ellipse(mouseX - 7, mouseY, 40);
+  pop();
+  
+  push();
+  strokeWeight(5);
+  line(218, 50, 183, 50);
+  pop();
 
+  push();
+  fill(255, 100, 100);
+  noStroke();
+  ellipse(mouseX - 10, mouseY, 20);
+  pop();
+
+  fill(0);
+  textAlign(CENTER);
+  textStyle(BOLD);
+  textSize(32);
+  push();
+  textSize(20);
+  text("yoyo simulator", 90, 35);
+  pop();
 }
+
